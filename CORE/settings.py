909f88f3b239f796e35876cd778225745b4983a9
asranet <info@asranet.co.uk>
2016-07-04 11:14:34 +0100

    Added 404 and other conferences pages

--- CORE/settings.py
+++ CORE/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ['core_key']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['www.offshore-renewables.co.uk', 'offshore-renewables.co.uk', 'localhost']
 
